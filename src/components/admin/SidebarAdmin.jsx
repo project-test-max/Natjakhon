864921dbd9023de9114cb824e503279b0ebabf00
project-test-max <p51maxzxc@gmail.com>
2025-01-20 13:53:41 +0700

    deploy

--- src/components/admin/SidebarAdmin.jsx
+++ src/components/admin/SidebarAdmin.jsx
@@ -23,18 +23,6 @@ const SidebarAdmin = () => {
 
       <nav className="flex-1 px-4 py-4 space-y-2">
         <NavLink
-          to={"/admin"}
-          end
-          className={({ isActive }) =>
-            isActive
-              ? "bg-gray-900 rounded-md text-white px-4 py-2 flex items-center"
-              : "text-gray-300 px-4 py-2 hover:bg-gray-700 hover:text-white rounded flex items-center"
-          }
-        >
-          <LayoutDashboard className="mr-2" />
-          Dashboard
-        </NavLink>
-        <NavLink
           to={"manage"}
           className={({ isActive }) =>
             isActive
